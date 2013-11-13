fae02cf1110ecbb29f36ff1ad289ee08ab3747ef
Jorik <jorik12@hotmail.com>
2013-11-13 12:45:45 +0100

    base rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vivorbi::Application.config.secret_key_base = 'baba790cfa702e1464d7374a56d11c2ffef876dd1fdf234e192f2d981c1e8181a11a70576f947603751d5b0d4f0294a82a36a9d06a84511d757ba74186588a89'

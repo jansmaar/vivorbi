4abe249d8e24f0ce72213284023a7226802b2539
Jorik <jorik12@hotmail.com>
2013-11-20 15:05:00 +0100

    Login Users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to events_path, :notice => "Signed up!"
+    else
+      render "new"
+    end
+    end
+    
+   def index
+   
+   end
+   
+   private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :salt, :encrypted_password)
+  end
+  end

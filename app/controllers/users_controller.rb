c9ea49d26af8dfca95895776caa0d68012b02837
Nick de Vries <nick.vries@gmail.com>
2013-12-11 14:49:22 +0100

    Users gegevens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,6 @@ class UsersController < ApplicationController
    private
 
   def user_params
-params.require(:user).permit(:email, :password, :salt, :encrypted_password, :firstname, :lastname, :avatar)  
+params.require(:user).permit(:email, :password, :salt, :encrypted_password, :firstname, :lastname, :avatar, :date_of_birth)  
   end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131210101016) do
+ActiveRecord::Schema.define(version: 20131211132008) do
 
   create_table "events", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20131210101016) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.date     "date_of_birth"
   end
 
 end

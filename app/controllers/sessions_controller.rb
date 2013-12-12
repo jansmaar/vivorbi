2ad1d0452723208f5ddc625ccbc3d74c27de674d
Stephan Westra <steffour@hotmail.com>
2013-12-12 10:24:04 +0100

    Flashmessage vertaling, stijl toegevoegd aan flashmessage h4 toegepast op startpagina

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,17 @@ class SessionsController < ApplicationController
   
 def destroy
   session[:user_id] = nil
-  redirect_to root_url, :notice => "Logged out!"
+  redirect_to root_url, :notice => "U bent succesvol uitgelogd!!"
 end
   
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to events_path, :notice => "Logged in!"
+      redirect_to events_path, :notice => "U bent succesvol ingelogd!"
+
     else
-      flash.now.alert = "Invalid email or password"
+      flash.now.alert = "U heeft uw gebruikersnaam of e-mailadres verkeerd ingevoerd!"
       render "new"
     end
    end

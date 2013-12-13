db828cd075938b63427dae3d73a2a114d627fe75
Jorik <jorik12@hotmail.com>
2013-12-13 13:44:29 +0100

    no message

--- app/controllers/particitpants_controller.rb
+++
@@ -1,3 +0,0 @@
-class ParticitpantsController < ApplicationController
-
-end
--- app/models/participant.rb
+++
@@ -1,4 +0,0 @@
-class Participant < ActiveRecord::Base
-	#belongs to :users#, class_name => "user"
-	#belongs to :events#, class_name => "event"
-end

432edcbc461d0e9163aac24edc70204273761be7
Stephan Westra <steffour@hotmail.com>
2013-12-13 12:02:08 +0100

    Google maps NL plaatsen bovenaan, number_to_phone, vertaling Signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to events_path, :notice => "Signed up!"
+      redirect_to events_path, :notice => "U bent succesvol aangemeld bij MetElkaar!"
     else
       render "new"
     end

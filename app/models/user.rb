98eedad3a6185ff2d55d5aa89f4e8c7489e8bf9b
Nick de Vries <nick.vries@gmail.com>
2013-12-17 11:06:23 +0100

    Email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
 
 def self.authenticate(email, password)
   user = find_by_email(email)
